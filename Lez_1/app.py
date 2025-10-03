be1c0f5d2b4e166c787d1c85643f5a53f43b8fa6
MatteoT <mtorchia@tai.it>
2025-10-03 18:24:10 +0200

    added tolower and toupper APIs

--- Lez_1/app.py
+++ Lez_1/app.py
@@ -1,10 +1,22 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__, instance_relative_config=True)
 
 @app.route('/hello', methods=['GET'])
 def hello():
     return jsonify(message="Hello world!")
+
+@app.route('/toupper', methods=['POST'])
+def to_upper():
+    data = request.get_json()
+    message = data.get('message', '')
+    return jsonify(message=message.upper())
+
+@app.route('/tolower', methods=['POST'])
+def to_lower():
+    data=request.get_json()
+    message=data.get('message','')
+    return jsonify(message=message.lower())
 
 if __name__ == '__main__':
     app.run(debug=True)
